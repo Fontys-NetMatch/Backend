8d5a79beb3e63a70b6373b4b3febacd36167ba99
Fabian Dingemans <mail@fabiandingemans.nl>
2025-03-13 14:08:26 +0100

    Added customer foreign key to quotation

---
+++ TravelPlanner.DB/Migrations/1741861218_CreateQuotationsTable.cs
@@ -0,0 +1,27 @@
+﻿using LinqToDB;
+using TravelPlanner.DB;
+using TravelPlanner.DB.Lib;
+using TravelPlanner.DB.Lib.MigrationsManager;
+using TravelPlanner.Domain.Models.Entities;
+
+namespace TravelPlanner.DB.Migrations;
+
+public class CreateQuotationsTable : IMigration
+{
+    public void Up(DbContext dbContext)
+    {
+        dbContext.CreateTable<Quotation>(tableOptions: TableOptions.CheckExistence);
+
+        var command = dbContext.CreateCommand();
+        // create foreign key between Quotations CustomerId and Customers Id
+        command.CommandText = @"
+            ALTER TABLE Quotations
+            ADD CONSTRAINT FK_Quotations_Customers
+            FOREIGN KEY (CustomerId)
+            REFERENCES Customers(Id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE";
+        command.ExecuteNonQuery();
+
+    }
+}
--- TravelPlanner.Domain/Models/Entities/Quotation.cs
+++ TravelPlanner.Domain/Models/Entities/Quotation.cs
@@ -1,23 +1,23 @@
 ﻿using LinqToDB.Mapping;
 
-namespace TravelPlanner.Domain.Models.Entities
+namespace TravelPlanner.Domain.Models.Entities;
+
+[Table("Quotations")]
+public record Quotation
 {
-    [Table("Quotations")]
-    public record Quotation
-    {
-        [Column, PrimaryKey, Identity]
-        public int Id { get; set; }
+    [Column, PrimaryKey, Identity]
+    public int Id { get; set; }
 
-        [Column(Length = 255), NotNull]
-        public required string Name { get; set; }
+    [Column(Length = 255), NotNull]
+    public required string Name { get; set; }
 
-        [Column, NotNull]
-        public required bool IsActive { get; set; }
+    [Column, NotNull]
+    public required bool IsActive { get; set; }
 
-        [Column, NotNull]
-        public required int CustomerId { get; set; }
+    [Column, NotNull]
+    public required int CustomerId { get; set; }
 
-        [Association(ThisKey = nameof(CustomerId), OtherKey = nameof(Customer.Id))]
-        public required Customer Customer { get; set; }
-    }
+    [Association(ThisKey = nameof(CustomerId), OtherKey = nameof(Customer.Id), CanBeNull = false)]
+    public required Customer Customer { get; set; }
+
 }
