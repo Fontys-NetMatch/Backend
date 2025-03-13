c6e218fab30b03e5a80541ed2349c6e7578120b2
Timdie <154594158+Timd1e@users.noreply.github.com>
2025-03-13 13:44:20 +0100

    Update DbManager.cs

--- TravelPlanner.DB/DbManager.cs
+++ TravelPlanner.DB/DbManager.cs
@@ -8,5 +8,8 @@ public class DbManager : DbContext
 {
 
     public ITable<User> Users => this.GetTable<User>();
+    public ITable<Product> Products => this.GetTable<Product>();
+    public ITable<Customer> Customers => this.GetTable<Customer>();
+    public ITable<Quotation> Quotations => this.GetTable<Quotation>();
 
 }
