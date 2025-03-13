8488a7f657550ebcc2a51fb3df39192834985dfa
Fabian Dingemans <mail@fabiandingemans.nl>
2025-03-13 11:05:59 +0100

    Added login data object response types

---
+++ TravelPlanner.DB/Migrations/1740648401_CreateProductsTable.cs
@@ -0,0 +1,15 @@
+﻿using LinqToDB;
+using TravelPlanner.DB;
+using TravelPlanner.DB.Lib;
+using TravelPlanner.DB.Lib.MigrationsManager;
+using TravelPlanner.Domain.Models.Entities;
+
+namespace TravelPlanner.DB.Migrations;
+
+public class CreateProductsTable: IMigration
+{
+    public void Up(DbContext dbContext)
+    {
+        dbContext.CreateTable<Product>(tableOptions: TableOptions.CheckExistence);
+    }
+}
