c05b08385e9310e72f34497ad22c412d27ab638e
Timdie <154594158+Timd1e@users.noreply.github.com>
2025-03-14 14:06:15 +0100

    AddonDate migration fixed and MigrationManager sort bug fixed

---
+++ TravelPlanner.DB/Migrations/1741956477_CreateAddonDatesTable.cs
@@ -0,0 +1,40 @@
+﻿using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPlanner.DB.Lib.MigrationsManager;
+using TravelPlanner.DB.Lib;
+using TravelPlanner.Domain.Models.Entities.Products;
+using TravelPlanner.Domain.Models.Entities;
+
+namespace TravelPlanner.DB.Migrations
+{
+    public class CreateAddonDatesTable : IMigration
+    {
+        public void Up(DbContext dbContext)
+        {
+            dbContext.CreateTable<AddonDate>(tableOptions: TableOptions.CheckExistence);
+
+            var command = dbContext.CreateCommand();
+
+            command.CommandText = @"
+            ALTER TABLE AddonDates
+            ADD CONSTRAINT FK_AddonDates_ProductDates
+            FOREIGN KEY (ProductDate_ID)
+            REFERENCES ProductDates(ID)";
+            command.ExecuteNonQuery();
+
+            var command1 = dbContext.CreateCommand();
+
+            command1.CommandText = @"
+            ALTER TABLE AddonDates
+            ADD CONSTRAINT FK_AddonDates_ProductAddons
+            FOREIGN KEY (ProductAddon_ID)
+            REFERENCES ProductAddons(ID)";
+            command1.ExecuteNonQuery();
+
+        }
+    }
+}
