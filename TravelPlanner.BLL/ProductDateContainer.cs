1c761251224f5c3aad6285cacd1dd4ef7658bccb
Piter Zakko <i475005@fhict.nl>
2025-03-20 13:18:17 +0100

    Product s Container
    ProductAddon + productDate + productImage + productType

---
+++ TravelPlanner.BLL/ProductDateContainer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using TravelPlanner.DB;
+using TravelPlanner.Domain.Models.Entities.Products;
+
+namespace TravelPlanner.BLL
+{
+    public class ProductDateContainer
+    {
+        private readonly DbManager _db;
+
+        public ProductDateContainer(DbManager db)
+        {
+            _db = db;
+        }
+
+        public async Task<int> CreateProductDateAsync(ProductDate productDate)
+        {
+            if (productDate == null)
+            {
+                throw new ArgumentNullException(nameof(productDate), "ProductDate cannot be null");
+            }
+
+            var result = await _db.InsertWithInt32IdentityAsync(productDate);
+            if (result == 0)
+            {
+                throw new InvalidOperationException("Failed to create ProductDate in the database");
+            }
+
+            return result;
+        }
+
+        public async Task<ProductDate?> GetProductDateByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ProductDate ID must be positive", nameof(id));
+            }
+
+            return await _db.ProductDates.FirstOrDefaultAsync(pd => pd.ID == id);
+        }
+
+        public async Task UpdateProductDateAsync(ProductDate productDate)
+        {
+            if (productDate == null)
+            {
+                throw new ArgumentNullException(nameof(productDate), "ProductDate cannot be null");
+            }
+
+            if (productDate.ID <= 0)
+            {
+                throw new ArgumentException("ProductDate must have a valid ID", nameof(productDate));
+            }
+
+            var result = await _db.UpdateAsync(productDate);
+            if (result == 0)
+            {
+                throw new InvalidOperationException("Failed to update ProductDate");
+            }
+        }
+
+        public async Task<IEnumerable<ProductDate>> GetAllActiveProductDatesAsync()
+        {
+            var productDates = await _db.ProductDates
+                                  .Where(pd => pd.IsActive)
+                                  .ToListAsync();
+
+            if (productDates == null || !productDates.Any())
+            {
+                throw new InvalidOperationException("No active ProductDates found");
+            }
+
+            return productDates;
+        }
+
+        public async Task SoftDeleteProductDateAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ProductDate ID must be positive", nameof(id));
+            }
+
+            var productDate = await GetProductDateByIdAsync(id);
+            if (productDate == null)
+            {
+                throw new InvalidOperationException("ProductDate does not exist and cannot be soft-deleted");
+            }
+
+            if (!productDate.IsActive)
+            {
+                throw new InvalidOperationException("ProductDate is already inactive");
+            }
+
+            productDate.IsActive = false;
+            await UpdateProductDateAsync(productDate);
+        }
+    }
+}
---
+++ TravelPlanner.BLL/ProductTypeContainer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using TravelPlanner.DB;
+using TravelPlanner.Domain.Models.Entities.Products;
+
+namespace TravelPlanner.BLL
+{
+    public class ProductTypeContainer
+    {
+        private readonly DbManager _db;
+
+        public ProductTypeContainer(DbManager db)
+        {
+            _db = db;
+        }
+
+        public async Task<int> CreateProductTypeAsync(ProductType productType)
+        {
+            if (productType == null)
+            {
+                throw new ArgumentNullException(nameof(productType), "ProductType cannot be null");
+            }
+
+            var result = await _db.InsertWithInt32IdentityAsync(productType);
+            if (result == 0)
+            {
+                throw new InvalidOperationException("Failed to create ProductType in the database");
+            }
+
+            return result;
+        }
+
+        public async Task<ProductType?> GetProductTypeByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ProductType ID must be positive", nameof(id));
+            }
+
+            return await _db.ProductTypes.FirstOrDefaultAsync(pt => pt.ID == id);
+        }
+
+        public async Task UpdateProductTypeAsync(ProductType productType)
+        {
+            if (productType == null)
+            {
+                throw new ArgumentNullException(nameof(productType), "ProductType cannot be null");
+            }
+
+            if (productType.ID <= 0)
+            {
+                throw new ArgumentException("ProductType must have a valid ID");
+            }
+
+            var result = await _db.UpdateAsync(productType);
+            if (result == 0)
+            {
+                throw new InvalidOperationException("Failed to update ProductType");
+            }
+        }
+
+        public async Task<IEnumerable<ProductType>> GetAllActiveProductTypesAsync()
+        {
+            var productTypes = await _db.ProductTypes
+                                  .Where(pt => pt.IsActive)
+                                  .ToListAsync();
+
+            if (productTypes == null || !productTypes.Any())
+            {
+                throw new InvalidOperationException("No active ProductTypes found");
+            }
+
+            return productTypes;
+        }
+
+        public async Task SoftDeleteProductTypeAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ProductType ID must be positive", nameof(id));
+            }
+
+            var productType = await GetProductTypeByIdAsync(id);
+            if (productType == null)
+            {
+                throw new InvalidOperationException("ProductType does not exist and cannot be soft-deleted");
+            }
+
+            if (!productType.IsActive)
+            {
+                throw new InvalidOperationException("ProductType is already inactive");
+            }
+
+            productType.IsActive = false;
+            await UpdateProductTypeAsync(productType);
+        }
+    }
+}
