078ba2dc51d62098f217467f39099f517250f028
Piter Zakko <i475005@fhict.nl>
2025-03-20 15:18:45 +0100

    ProductAddon class

---
+++ TravelPlanner.Domain/Models/Entities/Translations/ProductAddonTranslation.cs
@@ -0,0 +1,25 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPlanner.Domain.Models.Entities.Translations
+{
+    [Table("ProductAddonTranslation")]
+    public class ProductAddonTranslation
+    {
+        [Column, PrimaryKey, Identity]
+        public int ID { get; set; }
+
+        [Column(Length = 100), NotNull]
+        public int Name { get; set; }
+
+        [Column, NotNull]
+        public string Description { get; set; }
+
+        [Column(Length = 10), NotNull]
+        public string LangIsoCode { get; set; }
+    }
+}
