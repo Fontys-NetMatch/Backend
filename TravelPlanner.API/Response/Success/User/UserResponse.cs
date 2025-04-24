93ab449d452af851ca477064c4ca85062c884d4a
Timdie <154594158+Timd1e@users.noreply.github.com>
2025-04-24 10:19:26 +0200

    UserResponse errors fixed

--- TravelPlanner.API/Response/Success/User/UserResponse.cs
+++ TravelPlanner.API/Response/Success/User/UserResponse.cs
@@ -1,26 +1,23 @@
-﻿using TravelPlanner.Domain.Enums;
+﻿using TravelPlanner.API.Response;
+using TravelPlanner.Domain.Models.Entities;
 
-namespace TravelPlanner.API.Response.Success.User
+public record UserResponse : BaseResponse
 {
-    public record UserResponse : BaseResponse  
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string SurName { get; set; }
-        public string Email { get; set; }
-        public string ProfileImagePath { get; set; }
-        public bool IsActive { get; set; }
-        public string Password { get; set; }
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string ProfileImagePath { get; set; }
+    public bool IsActive { get; set; }
+    public string Password { get; set; }
 
-        public UserResponse(int id, string firstName, string surname, string email, string profileImagePath, bool isActive, string password)
-        {
-            Id = id;
-            FirstName = firstName;
-            SurName = surname;
-            Email = email;
-            ProfileImagePath = profileImagePath;
-            IsActive = isActive;
-            Password = password;
-        }
+    public UserResponse(int id, string firstname, string surname, string email, string profileImagePath, bool isActive)
+    {
+        Id = id;
+        Firstname = firstname;
+        Surname = surname;
+        Email = email;
+        ProfileImagePath = profileImagePath;
+        IsActive = isActive;
     }
 }
