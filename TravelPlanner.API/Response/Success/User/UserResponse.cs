def06c9843fa7e5b3a5ac3c02a805490731dfea0
Timdie <154594158+Timd1e@users.noreply.github.com>
2025-04-24 09:52:12 +0200

    User(s)Response created

--- TravelPlanner.API/Response/Success/User/UserResponse.cs
+++ TravelPlanner.API/Response/Success/User/UserResponse.cs
@@ -1,6 +1,26 @@
-﻿namespace TravelPlanner.API.Response.Success.User
+﻿using TravelPlanner.Domain.Enums;
+
+namespace TravelPlanner.API.Response.Success.User
 {
-    public class UserResponse
+    public record UserResponse : BaseResponse  
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string ProfileImagePath { get; set; }
+        public bool IsActive { get; set; }
+        public string Password { get; set; }
+
+        public UserResponse(int id, string firstName, string surname, string email, string profileImagePath, bool isActive, string password)
+        {
+            Id = id;
+            FirstName = firstName;
+            SurName = surname;
+            Email = email;
+            ProfileImagePath = profileImagePath;
+            IsActive = isActive;
+            Password = password;
+        }
     }
 }
--- TravelPlanner.API/Response/Success/User/UsersResponse.cs
+++ TravelPlanner.API/Response/Success/User/UsersResponse.cs
@@ -1,6 +1,14 @@
-﻿namespace TravelPlanner.API.Response.Success.User
+﻿using TravelPlanner.API.Response.Success.ProductType;
+
+namespace TravelPlanner.API.Response.Success.User
 {
-    public class UserResponses
+    public record UsersResponse : BaseResponse
     {
+        public List<UserResponse> Users { get; set; }
+
+        public UsersResponse(List<UserResponse> users)
+        {
+            Users = users;
+        }
     }
 }
