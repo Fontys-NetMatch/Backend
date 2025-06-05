02b36daffd42296aeb7ed6e7a020d7935a43f741
Ihavenojoy <tom.van.de.lest@gmail.com>
2025-06-05 13:31:55 +0200

    update interfaces

--- TravelPlanner.Domain/Interfaces/BLL/Container/IProductAddonTranslationContainer.cs
+++ TravelPlanner.Domain/Interfaces/BLL/Container/IProductAddonTranslationContainer.cs
@@ -1,6 +1,12 @@
-﻿namespace TravelPlanner.Domain.Interfaces.BLL.Container;
+﻿using TravelPlanner.Domain.Models.Entities;
+
+namespace TravelPlanner.Domain.Interfaces.BLL.Container;
 
 public interface IProductAddonTranslationContainer
 {
-    
+    Task<int> CreateProductAddonTranslationAsync(ProductAddonTranslation productAddonTranslation);
+    Task<ProductAddonTranslation?> GetProductAddonTranslationByIdAsync(int id);
+    Task UpdateProductAddonTranslationAsync(ProductAddonTranslation productAddonTranslation);
+    Task<IEnumerable<ProductAddonTranslation>> GetAllTranslationsByLangIsoCodeAsync(string langIsoCode);
+    Task DeleteProductAddonTranslationAsync(int id);
 }
